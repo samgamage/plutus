df8f79dbc879583e5f75a0848a58514c2e572a68
James Breedlove <qslick04@gmail.com>
2019-10-26 02:19:30 -0400

    Working on Redux

--- App.js
+++ App.js
@@ -2,23 +2,24 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import AppNavigator from "./navigators/AppNavigator";
+import store from './redux/store';
 
 const initialState = {
   action: "",
   name: ""
 };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    // define actions here e.g.,
-    // case "OPEN_MENU":
-    //   return { action: "openMenu" };
-    default:
-      return state;
-  }
-};
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     // define actions here e.g.,
+//     // case "OPEN_MENU":
+//     //   return { action: "openMenu" };
+//     default:
+//       return state;
+//   }
+// };
 
-const store = createStore(reducer);
+// const store = createStore(reducer);
 
 const App = () => (
   <Provider store={store}>
